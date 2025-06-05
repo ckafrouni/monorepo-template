64730589906c6f78f9f3e86d7995cc557f161076
Christophe Kafrouni <44137603+ckafrouni@users.noreply.github.com>
2025-06-05 23:11:56 +0200

    feat: consolidate sign-in and sign-up forms into a single LoginForm component, enhancing user experience and simplifying code structure

--- packages/app/src/routes/login.tsx
+++ packages/app/src/routes/login.tsx
@@ -1,18 +1,23 @@
-import SignInForm from '@/components/sign-in-form';
-import SignUpForm from '@/components/sign-up-form';
 import { createFileRoute } from '@tanstack/react-router';
-import { useState } from 'react';
+import { GalleryVerticalEnd } from 'lucide-react';
+import { LoginForm } from '@/components/login-form';
 
 export const Route = createFileRoute('/login')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	const [showSignIn, setShowSignIn] = useState(false);
-
-	return showSignIn ? (
-		<SignInForm onSwitchToSignUp={() => setShowSignIn(false)} />
-	) : (
-		<SignUpForm onSwitchToSignIn={() => setShowSignIn(true)} />
+	return (
+		<div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+			<div className="flex w-full max-w-sm flex-col gap-6">
+				<a href="#" className="flex items-center gap-2 self-center font-medium">
+					<div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+						<GalleryVerticalEnd className="size-4" />
+					</div>
+					Acme Inc.
+				</a>
+				<LoginForm />
+			</div>
+		</div>
 	);
 }
